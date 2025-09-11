cb95811492b39ba2f42c92b5b012f4ddb3fed787
Leo R <leoreyes_07@outlook.com>
2025-09-10 21:46:16 -0600

    Added hoistin and debugger

---
+++ src/hoiting/index.js
@@ -0,0 +1,12 @@
+/* var nameOfDog;
+console.log(nameOfDog);
+var nameOfDog = 'Firu';
+console.log(nameOfDog);
+ */
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${Firu}`);
+    
+}
+var Firu = 'Firulais';
